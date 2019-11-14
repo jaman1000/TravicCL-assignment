c7f15f2da8c25118e27f302f296da2ae5b555c61
jaman1000 <javonelliott28@gmail.com>
2019-11-14 15:12:58 -0500

    failed test

--- src/test/java/addtest.java
+++ src/test/java/addtest.java
@@ -7,7 +7,7 @@ public class addtest {
         @Test
         public void addtest(){
         add addition = new add();
-        assertEquals(14, add.add(5,9));
+        assertEquals(17, add.add(5,9));
 
         }
 }
